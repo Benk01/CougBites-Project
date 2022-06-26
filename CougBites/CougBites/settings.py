c9ae5711cacda6e8b4d9c7d1804985b0deaf37a8
Ben Kaufmann <benk01@hotmail.com>
2022-06-26 13:34:49 -0700

    updated register form

--- CougBites/CougBites/settings.py
+++ CougBites/CougBites/settings.py
@@ -37,7 +37,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'core'
+    'core.apps.CoreConfig',
+    'crispy_forms'
 ]
 
 MIDDLEWARE = [
@@ -126,3 +127,5 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+CRISPY_TEMPLATE_PACKAGE="bootstrap5"
